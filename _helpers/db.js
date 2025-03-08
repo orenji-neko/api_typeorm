d5904258f24113c6393b948746020a29df1348e6
Mark Enfermo <112786278+orenji-neko@users.noreply.github.com>
2025-03-08 16:54:21 +0800

    init

---
+++ _helpers/db.js
@@ -0,0 +1,19 @@
+const config = require('config.json');
+const mysql = require('mysql2/promise');
+const { Sequalize} = require('sequelize');
+
+module.exports = db = {};
+
+initialize();
+
+async function initialize() {
+    const { host, port, user, password, database} = config.database;
+    const connection = await mysql.createConnection({ host, port, user, password});
+    await connection.query('CREATE DATABASE IF NOT EXISTS \'${database}\';');
+
+    const sequelize = new Sequalize(database, user, password, {dialect: 'mysql'});
+
+    db.User = require ('../user/user.model')(sequelize);
+
+    await sequelize.sync({alter: true});
+}
